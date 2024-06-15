d728763197977326a486cce12b742943041eda7e
simondv4 <simondv4@gmail.com>
2024-06-15 17:54:40 +0300

    test

--- module_2_6.py
+++ module_2_6.py
@@ -7,4 +7,4 @@ def single_root_words(root_word, *other_word):
 
 
 print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
-print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
+# print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
